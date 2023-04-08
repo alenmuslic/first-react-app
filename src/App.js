1625e2e2aa80f717f731b266b525896ef2843903
alenmuslic <muslicalen15@gmail.com>
2023-04-08 15:04:34 +0200

    React Kartice

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 // import logo from "./logo.svg";
-import "./App.css";
-import Greeting from "./components/Greeting";
+// import "./App.css";
+// import Greeting from "./components/Greeting";
+// import Greeting from "./components/Greeting/Greeting";
+// import PersonCard from "./components/Cards/PersonCard";
+import MojaKartica from "./components/mojRad/MojaKartica";
+
+// function App() {
+// return (
+//     <>
+//       <div className="App">
+//         {/* <header className="header">
+//           <img src="https://duomly.nyc3.digitaloceanspaces.com/articles/coding/alps-lake.jpg" />
+//           <h1 style={{ color: "green" }}>Prvi radovi u React Project</h1>
+//         </header> */}
+//         {/* <Greeting appName={"MyFirstAppReact"} username={"."} /> */}
+//         <PersonCard />
+//       </div>
+//     </>
+//   );
+// }
 
 function App() {
   return (
     <>
-      <div className="App">
-        <header className="header">
-          {/* <img src="https://duomly.nyc3.digitaloceanspaces.com/articles/coding/alps-lake.jpg" /> */}
-          <h1 style={{ color: "green" }}>Prvi radovi u React Project</h1>
-        </header>
-        <Greeting appName={"MyFirstAppReact"} username={"."} />
+      <div
+        style={{
+          width: "90%",
+          display: "grid",
+          gridTemplateColumns: "repeat(4,250px)",
+          gridGap: "100px",
+          // gridAutoRows: "minmax(420px,auto)",
+          justifyContent: "center",
+        }}
+      >
+        <MojaKartica imgUrl />
+        <MojaKartica />
+        <MojaKartica />
+        <MojaKartica />
+        <MojaKartica />
+        <MojaKartica />
+        <MojaKartica />
+        <MojaKartica />
       </div>
     </>
   );
