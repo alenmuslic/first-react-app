adfeda7c4789d295809b05c52f0ffdb1c170e237
alenmuslic <muslicalen15@gmail.com>
2023-04-10 23:41:10 +0200

    react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import "./App.css";
-// import Greeting from "./components/Greetings/Greeting";
-// import { Navbar } from "./components/Navbar/Navbar";
-import MojJs from "./components/prviProjekat/MojJs";
-import informacije from "./common1/informacije.json";
+import Greeting from "./components/Greetings/Greeting";
+import { Navbar } from "./components/Navbar/Navbar";
+import PersonCard from "./components/Cards/PersonCard/PersonCard";
+import persons from "./common/persons.json";
+import hotels from "./common/hotels.json";
+import HotelCard from "./components/Cards/HotelCard/HotelCard";
 
 // const persons = [
 //   {
@@ -48,33 +50,69 @@ function App() {
     setCount(count - 1);
   };
   // const x = 10;
+
+  const [name, setName] = useState("");
+  // console.log(name);
   return (
     //  React.createElement("p", {}, "Neki paragraf");
     <>
       {" "}
       {/* Fragment - najcesce se koristi za wrappovanje */}
       <div className="App">
-        {/* <Navbar><p>Samo za primer</p></Navbar> */}
-        {/* <Greeting appName={"Our First App"} username={"Bakir Ujkanovic"} /> */}
+        <Navbar>{/* <p>Samo za primer</p> */}</Navbar>
+        <Greeting appName={"Our First App"} username={"Bakir Ujkanovic"} />
         <div
           style={{
             display: "grid",
-            gridTemplateColumns: "repeat(3, 380px)",
+            gridTemplateColumns: "repeat(4, 250px)",
             justifyContent: "center",
-            gridAutoRows: "minmax(280px, auto)",
-            gridGap: "50px",
-            columnGap: "30px",
+            gridAutoRows: "minmax(420px, auto)",
+            gridGap: "40px",
           }}
         >
-          {informacije.map((info) => (
-            <MojJs
-              imageURL={info.imageURL}
-              name={info.name}
-              stars={info.stars}
-              rating={info.rating}
-              reviews={info.reviews}
-              ocena={info.ocena}
-              komentar={info.komentar}
+          {/* <PersonCard
+            imageURL={"https://avatars.githubusercontent.com/u/89378479?v=4"}
+            fullName={"Dzenan Kosuta"}
+            location={"Novi pazar, Serbia"}
+            description={
+              "Dženan is a mathematician, and he work's in center NIT as a Webdeveloper"
+            }
+            goToRepositories={
+              "https://github.com/dzenankosuta?tab=repositories"
+            }
+          />
+          <PersonCard
+            imageURL={"https://avatars.githubusercontent.com/u/111905831?v=4"}
+            fullName={"Alen Muslic"}
+            location={"Novi pazar, Serbia"}
+            description={"Alen is rising Web developer..."}
+            goToRepositories={"https://github.com/alenmuslic?tab=repositories"}
+          />
+          <PersonCard
+            imageURL={
+              "https://www.borisradivojkov.com/assets/images/profesionalni-poslovni-portret-rukovodioca-600x600.jpg"
+            }
+            fullName={"Aladin Zecic"}
+            location={"Novi pazar, Serbia"}
+            description={"Aladin is rising Web developer..."}
+            goToRepositories={"https://github.com/aladinzecic?tab=repositories"}
+          />
+          <PersonCard
+            imageURL={"https://avatars.githubusercontent.com/u/111905979?v=4"}
+            fullName={"Haris Muslic"}
+            location={"Novi pazar, Serbia"}
+            description={"Haris is rising Web developer..."}
+            goToRepositories={
+              "https://github.com/harismuslic04?tab=repositories"
+            }
+          /> */}
+          {persons.map((person) => (
+            <PersonCard
+              imageURL={person.imageURL}
+              fullName={person.fullName}
+              location={person.location}
+              description={person.description}
+              goToRepositories={person.goToRepositories}
             />
           ))}
           <div>
@@ -92,6 +130,89 @@ function App() {
               +
             </button>
           </div>
+        </div>
+        <div className="hotels">
+          {hotels.map((hotel) => (
+            <HotelCard
+              imageURL={hotel.imageURL}
+              name={hotel.name}
+              stars={hotel.stars}
+              description={hotel.description}
+              rating={hotel.rating}
+              reviews={hotel.reviews}
+            />
+          ))}
+        </div>
+        <div className="formContainer">
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+            }}
+          >
+            <label htmlFor="firstName">Unesite vase ime</label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              required
+              // innerText={"ime"}
+              value={name}
+              onChange={(event) => {
+                console.log(event);
+                setName(event.target.value);
+              }}
+            />
+            <br />
+            <br />
+
+            <label htmlFor="lastName">Unesite vase prezime</label>
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              required
+              value={"ime"}
+              onChange={() => {}}
+            />
+            <br />
+            <br />
+
+            <label htmlFor="email">Unesite vasu email adresu</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              required
+              value={"ime"}
+              onChange={() => {}}
+            />
+            <br />
+            <br />
+
+            <label htmlFor="hobi">Unesite vas hobi</label>
+            <input
+              type="text"
+              id="hobi"
+              name="hobi"
+              value={"ime"}
+              onChange={() => {}}
+            />
+            <br />
+            <br />
+
+            <label htmlFor="phone">Unesite vas broj telefona</label>
+            <input
+              type="tel"
+              id="phone"
+              name="phone"
+              value={"ime"}
+              onChange={() => {}}
+            />
+            <br />
+            <br />
+
+            <button type="submit">Potvrdi</button>
+          </form>
         </div>
       </div>
     </>
--- src/components/Greetings/Greeting.js
+++ src/components/Greetings/Greeting.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Greeting.css";
+
 function Greeting(props) {
-  console.log(props.username);
+  console.log(props);
   console.log(props.appName);
   return (
     <main className="main">
-      <h2>Dobrodosli u {props.appName}</h2>
-      <h3>Hvala sto ste nas posetili {props.username}</h3>
+      <h2>Dobro dosli u {props.appName}!</h2>
+      <h3>Hvala sto ste nas posetili, {props.username}.</h3>
     </main>
   );
 }
