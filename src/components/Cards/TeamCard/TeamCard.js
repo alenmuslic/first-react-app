a1683d44a07105d61a53dfedf58f6a2dce807621
alenmuslic <muslicalen15@gmail.com>
2023-05-03 23:54:48 +0200

    changes

--- src/components/Cards/TeamCard/TeamCard.js
+++ src/components/Cards/TeamCard/TeamCard.js
@@ -1,15 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./TeamCard.css";
+import { BASE_URL } from "../../../App";
 
-function TeamCard(props) {
+function TeamCard({
+  name,
+  matches,
+  points,
+  deleteTeam,
+  extended,
+  setExtended,
+  information,
+  id,
+}) {
+  const [closed, setClosed] = useState(true);
+  // console.log({ BASE_URL }); radi primera
+  useEffect(() => {
+    if (extended !== null && extended !== id) {
+      setClosed(true);
+    }
+  }, [extended]);
+
+  useEffect(() => {
+    if (!closed) {
+      setExtended(id);
+    } else {
+      setExtended(null);
+    }
+  }, [closed]);
   return (
     <div className="cardContainer2">
       <div className="card2">
-        <p>{props.name}sdf</p>
-        <p>{props.matches}sdfsdf</p>
-        <p>{props.points}sdfsdf</p>
-        <button onClick={props.deleteTeam}>Izbrisi tim</button>
+        <p>{name}</p>
+        <p>Broj odigranih meceva: {matches}</p>
+        <p>Broj poena: {points}</p>
+        <button onClick={deleteTeam}>Izbrisi tim</button>
+        <button
+          onClick={() => {
+            setClosed(!closed);
+          }}
+        >
+          {closed ? <span>Prikazi vise</span> : <span>Prikazi manje</span>}
+        </button>
       </div>
+      {!closed && (
+        <div className="info">
+          <p>{information}</p>
+        </div>
+      )}
     </div>
   );
 }
